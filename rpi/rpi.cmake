0038a6faf7d84444f8abc298b3c14e3576a5c5fb
Anatoly Kupriyanov <kan.izh@gmail.com>
2015-02-26 23:44:53 +0000

    RPi hello-world

---
+++ rpi/rpi.cmake
@@ -0,0 +1,10 @@
+SET(RPI_X_COMPILE_DIR ${HAL1_SOURCE_DIR}/3rd/rpi)
+
+SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_SYSTEM_VERSION 1)
+SET(CMAKE_C_COMPILER ${RPI_X_COMPILE_DIR}/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-gcc)
+SET(CMAKE_CXX_COMPILER ${RPI_X_COMPILE_DIR}/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/arm-linux-gnueabihf-g++)
+SET(CMAKE_FIND_ROOT_PATH ${RPI_X_COMPILE_DIR}/rootfs)
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
